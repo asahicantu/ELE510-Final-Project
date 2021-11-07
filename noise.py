b5083c0ab323b11b5a3b55ea233db00b803fe4d8
Asahi Cantu <asahicantu@outlook.com>
2021-11-07 13:22:30 +0100

    Added noisy images

--- noise.py
+++ noise.py
@@ -2,7 +2,14 @@
 import numpy as np
 import os
 import cv2
-def noisy(noise_type,image):
+
+
+NOISE_TYPE_GAUSS = 'gauss'
+NOISE_TYPE_SALT_PEPPER = 's&p'
+NOISE_TYPE_POISSON = 'poisson'
+NOISE_TYPE_SPECKLE = 'speckle'
+
+def noisify(noise_type, image):
     """
     Parameters
     ----------
@@ -17,7 +24,7 @@ def noisy(noise_type,image):
         'speckle'   Multiplicative noise using out = image + n*image,where
                     n is uniform noise with specified mean & variance.
     """
-    if noise_type == "gauss":
+    if noise_type == NOISE_TYPE_GAUSS:
         row,col,ch= image.shape
         mean = 0
         var = 0.1
@@ -26,7 +33,7 @@ def noisy(noise_type,image):
         gauss = gauss.reshape(row,col,ch)
         noisy = image + gauss
         return noisy
-    elif noise_type == "s&p":
+    elif noise_type == NOISE_TYPE_SALT_PEPPER:
         row,col,ch = image.shape
         s_vs_p = 0.5
         amount = 0.004
@@ -41,14 +48,20 @@ def noisy(noise_type,image):
         coords = [np.random.randint(0, i - 1, int(num_pepper)) for i in image.shape]
         out[coords] = 0
         return out
-    elif noise_type == "poisson":
+    elif noise_type == NOISE_TYPE_POISSON:
         vals = len(np.unique(image))
         vals = 2 ** np.ceil(np.log2(vals))
         noisy = np.random.poisson(image * vals) / float(vals)
         return noisy
-    elif noise_type =="speckle":
-        row,col,ch = image.shape
-        gauss = np.random.randn(row,col,ch)
-        gauss = gauss.reshape(row,col,ch)        
+    elif noise_type == NOISE_TYPE_SPECKLE:
+        gauss = None
+        if len(image.shape) == 2:
+            row,col = image.shape #row,col & channel if more than 1 color channel present
+            gauss = np.random.randn(row,col)
+            gauss = gauss.reshape(row,col)        
+        elif len(image.shape) == 2:
+            row,col,ch = image.shape #row,col & channel if more than 1 color channel present
+            gauss = np.random.randn(row,col,ch)
+            gauss = gauss.reshape(row,col,ch)        
         noisy = image + image * gauss
         return noisy
